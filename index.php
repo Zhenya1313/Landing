3f98b28042292cef17aea3173f0a7ba3b175244d
Zhenya1313 <zhenya.bublik.2017@gmail.com>
2020-08-07 01:22:44 +0300

    1. Сверстал меню 2. Первую секцию 3. Начал верстать форму отправки сообщений

--- index.php
+++ index.php
@@ -9,25 +9,56 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="menu">
-    <div class="active">
-     <div class="home">Home</div>
+<div class="header">
+    <div id="menu" class="menu">
+        <div class="p_menu active">Home</div>
+        <div class="p_menu">Услуги</div>
+        <div class="p_menu">Контакты</div>
     </div>
-    <div class="services">Услуги</div>
-    <div class="contact">Контакты</div>
+    <div class="first_section">
+        <div class="headline">
+            <div class="title">Lorem ipsum</div>
+            <div class="pod_text">Lorem ipsum</div>
+        </div>
+        <div class="player">
+                <video class="video" src="" controls="controls" autoplay>
+                </video>
+        </div>
+    </div>
+    <div class="first_section">
+        <div class="send_form">
+            <form action="telegram.php" method="POST">
+                <div class="form-group">
+                    <input type="text" class="name" name="name" placeholder="Введите имя">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="phone" name="phone" placeholder="Введите телефон">
+                </div>
+                <div class="form-group">
+                    <textarea name="msg" class="message" placeholder="Введите сообщение"></textarea>
+                </div>
+                <button type="submit" class="btn">Отправить</button>
+            </form>
+        </div>
+    </div>
 </div>
 
-<!--<form action="telegram.php" method="POST">-->
-<!--    <div class="form-group">-->
-<!--        <input type="text" class="form-control" name="name" placeholder="Введите имя">-->
-<!--    </div>-->
-<!--    <div class="form-group">-->
-<!--        <input type="text" class="form-control" name="phone" placeholder="Введите телефон">-->
-<!--    </div>-->
-<!--    <div class="form-group">-->
-<!--        <textarea name="msg" placeholder="Введите сообщение"></textarea>-->
-<!--    </div>-->
-<!--    <button type="submit" class="btn btn-primary">Отправить</button>-->
-<!--</form>-->
+<div>
+
+</div>
+
+<script>
+    var btnContainer = document.getElementById("menu");
+    // Сделать все кнопки с class="btn" внутри контейнера
+    var btns = btnContainer.getElementsByClassName("p_menu");
+    // Выполните цикл по кнопкам и добавьте активный класс к текущей/нажатой кнопке
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+</script>
 </body>
 </html>
